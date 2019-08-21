69d2cff78db3fa58484d8902a67673c07eb46a05
Your Name <you@example.com>
2019-08-21 18:49:01 -0500

    add a quarter button and a timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -8,6 +8,20 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, setHomeScore] = useState(0);
 const [awayScore, setAwayScore] = useState(0);
+const [time, setTime] = useState(100);
+
+      useEffect(() => {
+          const timer = setInterval(() => {
+            setTime(pastTime => {
+              if (pastTime > 0) {
+                setTime(pastTime - 1);
+          }
+            })
+        }, 1000);
+        return () => {
+         clearInterval(timer);
+        };
+        }, []);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -19,12 +33,13 @@ const [awayScore, setAwayScore] = useState(0);
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{time}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
           </div>
-        </div>
+        </div>  
+
         <BottomRow />
       </section>
       <section className="buttons">
